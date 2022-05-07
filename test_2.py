24ea7cdae70cdc5badd8b27ac2b3c163ce2d0542
Lyle Buendia <lylebuendia@mail.fresnostate.edu>
2022-05-06 21:45:59 -0700

    finished structured english

---
+++ test_2.py
@@ -0,0 +1,21 @@
+
+
+
+"""
+The program is trying to display the number of grades, the average grades, and the percentage of grades
+that are above the average grade by using the scores listed in the Final.txt. There will two functions,
+function1 will initialize the application and function2 will calculate the percentage of grades that are 
+above the average grade.
+
+function1 will be the main function to initialize the application by assigning the grades in Final.txt into a list
+of integers and then calculating the average.
+funtion2 will calculate the percent above average by taking the count of tests taken divided by the grade average.
+
+The program will then ouput like so:
+Number of grades:
+
+Average grade:
+
+Percentage of grades above average: 
+
+"""
