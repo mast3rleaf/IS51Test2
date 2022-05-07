ec7199d266339d74dac06c6dd250037a063aaa82
Lyle Buendia <lylebuendia@mail.fresnostate.edu>
2022-05-06 22:42:43 -0700

    finished code

--- test_2.py
+++ test_2.py
@@ -42,3 +42,27 @@ avg /= len(grades)
 
 main
 """
+
+
+
+def calculate_percent_above_average(grades, avg):
+    count = 0
+    for grade in grades:
+        if (grade > avg):
+            count += 1
+    return ((count/len(grades))*100)
+
+def main():
+    f = open('Final.txt')
+    grades = []
+    for line in f:
+        grades.append(int(line.strip()))
+    print("Number of grades:", len(grades))
+    avg = 0
+    for grade in grades:
+        avg += grade 
+    avg = avg/len(grades)
+    print("Average grade:", avg)
+    print("Percentage of grades above average: {:.2f}%".format(calculate_percent_above_average(grades,avg)))
+
+main()
